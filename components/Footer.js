e48f35255158320fb3478c3d400d3d5a8a30a6a0
brown3qqq <brown3qqq@foxmail.com>
2021-06-15 00:52:23 +0800

    修改一下上传结构

--- components/Footer.js
+++
@@ -1,5 +0,0 @@
-export default () => (
-    <div className='footer'>
-        <div>在重新修改模板中，模板来自于github：inlife  <span className='smaller'><i className='fa fa-heart red'></i></span> 2021</div>
-    </div>
-)
