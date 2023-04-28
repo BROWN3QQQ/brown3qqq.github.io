6e46fd393e84259af4cf498c53fc0bda7f038f79
Qian Cui 2748642c <603856170@qq.com>
2023-04-28 16:07:07 +0100

    atart

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const name          = '崔谦 & Jackson'
 const role          = '软件工程师'
-const site          = 'http://localhost:3000/static/'
+const site          = 'https://brown3qqq.github.io/static/'
 const yearsOfExp    = (new Date()).getFullYear() - 2021
 const keywords      = ['inlife', 'inlife360'].concat(name.split(' '), role.split(' ')).join(' ')
 const description   = `Hey! I am ${name}, ${role} with >${yearsOfExp} years of experience.`;
